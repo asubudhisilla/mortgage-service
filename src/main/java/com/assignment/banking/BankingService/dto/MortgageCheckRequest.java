659e0de9b2ca665c6556fc640e59bab4365621ff
apple <amars400@gmail.com>
2025-07-04 11:51:56 +0200

    Junit for service class

--- src/main/java/com/assignment/banking/BankingService/dto/MortgageCheckRequest.java
+++ src/main/java/com/assignment/banking/BankingService/dto/MortgageCheckRequest.java
@@ -2,11 +2,13 @@ package com.assignment.banking.BankingService.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class MortgageCheckRequest {
     @NotNull
     @Min(1)
